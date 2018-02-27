6fea72f66ac0eff2d7f78d9f2479e43d2ad46e55
Dayvid <dayvid.blender@gmail.com>
2018-02-27 15:24:08 -0300

    inicial
    mult4051.cpp criado

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
-# smart-seedling
-Smart Seedling
+# Smart Seedling
+Smart Seedling é um projeto de código aberto que provê algumas funções básicas para um sistema de irrigação automatizado para mudas de plantas, você só precisa criar as regras para o seu tipo de horta, sem precisar se preocupar com coisas mais técnicas.
+
+## Começando
+
+Este projeto é derivado de um anterior, o Smart Garden, embora seja mais simples ele tem algo diferente do anterior. Nesse novo sistema foi desenvolvido a implementação com um multiplexador, para a leitura de mais sensores. Abaixo mostro como funciona a conexão de com o arduino.
+
+### Conectar com o arduino
+
+![alt tag](http://2.bp.blogspot.com/-Rb70EwOABZE/WpWafmWI_uI/AAAAAAAADxM/ikiXlRldTVcLsLL08iFRx71StUxHOLI5ACK4BGAYYCw/s1600/4051.jpg)
+
+Desenvolvi uma biblioteca onde substituimos o objeto "Sensor" pelo "MULT4051", que é responsável pela mesma tarefa do Sensor, só que agora funciona por meio de multiplexação.
+
+Para adicionar ao projeto é só incluir a "MULT4051.cpp" e substituir Sensor por MULT4051.
+
+```
+#include "MULT4051.cpp"
+
+```
+
+### Sobre a MULT4051.cpp
+
+Ela possui os mesmos métodos da Sensor, só que você terá que inserir alguns parãmetros. Ex:
+
+```
+sensores_solo.ler(false, false, true);
+
+```
+
+Esses parâmetros são responsáveis por setar qual sensor será lido, variando de (false, false, false) até (true, true, true).
+
+## Testes
+
+Antes de utilizar tudo em conjunto faça testes unitários.
---
+++ smart_seedling.ino
@@ -0,0 +1,89 @@
+#include <Bounce2.h>
+#include "rele.cpp";
+#include "sensor_umi_solo.cpp";
+#include "MULT4051.cpp"
+
+#define PIN_SOLENOIDE1 2 // Solenoide da caixa
+#define NIVEL_LOGICO_RELE_SOLENOIDE1 LOW
+
+#define PIN_BOMBA 3 // Solenoide da bomba
+#define NIVEL_LOGICO_RELE_BOMBA LOW
+
+#define SENSORES_UMI_SOLO A0
+
+#define SMS_VCC 7
+#define SMS_GND 6
+
+#define VALOR_SOLO_SECO 35
+
+Rele solenoide_caixa(PIN_SOLENOIDE1, NIVEL_LOGICO_RELE_SOLENOIDE1);
+Rele bomba(PIN_BOMBA, NIVEL_LOGICO_RELE_BOMBA);
+
+MULT4051 sensores_solo(SENSORES_UMI_SOLO, 9, 10, 11, SMS_VCC, SMS_GND);
+
+#define QUANT_SENSORES_SOLO 1
+
+#define TEMPO_ESPERA 3000
+ 
+Bounce debouncer = Bounce();
+
+int lerSensores(int);
+boolean verificarBoia();
+ 
+void setup() {
+  pinMode(SMS_VCC, OUTPUT);
+  pinMode(SMS_GND, OUTPUT); 
+  pinMode(SENSORES_UMI_SOLO, INPUT);
+  pinMode(PIN_SOLENOIDE1, OUTPUT);
+  pinMode(PIN_BOMBA, OUTPUT);
+  pinMode(9, OUTPUT);
+  pinMode(10, OUTPUT);
+  pinMode(11, OUTPUT);
+  pinMode(8, INPUT_PULLUP); // Configura pino 8 como entrada e habilita pull up interno;
+  debouncer.attach(8); // Informa que o tratamento de debouce será feito no pino 8;
+  debouncer.interval(10); // Seta o intervalo de trepidação;
+  Serial.begin(9600);
+}
+ 
+void loop() {
+  int valor = lerSensores(QUANT_SENSORES_SOLO); 
+  Serial.println(valor);
+  solenoide_caixa.desligar();
+  bomba.desligar();
+  
+  if (valor < VALOR_SOLO_SECO) {
+    solenoide_caixa.desligar();
+    bomba.ligar();
+    boolean boia = false;
+    while (boia == false) {
+       boia = verificarBoia();
+       delay(100);
+    }
+    bomba.desligar();
+    delay(TEMPO_ESPERA);
+    solenoide_caixa.ligar();
+    delay(TEMPO_ESPERA);
+  }
+  
+  delay(100);
+}
+
+boolean verificarBoia() {
+  debouncer.update();
+  int valor = debouncer.read();
+  if (valor == HIGH) {
+    return false;
+  } 
+  else {
+    return true;
+  }
+}
+
+int lerSensores(int quant_sensores) {
+  int soma = 0;
+  for (int i = 0; i < quant_sensores; i++) {
+    soma += sensores_solo.ler(false, false, true);
+  }
+  return soma / quant_sensores;
+}
+
